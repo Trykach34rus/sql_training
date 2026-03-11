41d08da75285ea7546ad4ba71c1468c5cbd029d9
Trykach34rus <smoke14071995Andres@gmail.com>
2026-03-11 18:29:49 +0300

    [add] postgreSQL

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"sql_training/postgres/simple_connection"
+)
+
 func main() {
-	// ctx := context.Background()
+	ctx := context.Background()
+
+	conn,err := simple_connection.CreateConnection(ctx)
+	if err != nil{
+		panic(err)
+	}
+	conn.Ping(ctx)
+	fmt.Println("Работает хреновина")
 
 }
